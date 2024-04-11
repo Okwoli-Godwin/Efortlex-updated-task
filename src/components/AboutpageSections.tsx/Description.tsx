336d452b8a599af350a2ff3bce05da34f3f47189
Okwoli-Godwin <okwolig60@gmail.com>
2024-04-11 16:13:57 +0100

    update

--- src/components/AboutpageSections.tsx/Description.tsx
+++ src/components/AboutpageSections.tsx/Description.tsx
@@ -14,21 +14,21 @@ const Description = () => {
             </div>
             
             <div className='w-[60%] md:w-[100%] sm:w-[100%]'>
-                <p className='text-justify max-md:text-[14px] leading-relaxed text-white'>
+                <p className='text-justify text-[18px] sm:text-[15px] max-md:text-[14px] leading-relaxed text-white'>
                     A-Class Academic Consults Limited is a consultancy, research and personnel training company that integrates cutting-edge technology to deliver practical solutions to the challenges of organisations in the private and public sectors. Founded and registered in Nigeria and with working experiences supporting clients all over the world. A-Class Academic Consults Limited strives to offer innovative sustainable solutions and improve organisational competencies amongst organisations in the private and public sectors. To achieve this, we offer noteworthy research, personnel/human development training, and consultancy services. 
                 </p>
-                <p className='mt-8 text-justify max-md:text-[14px] leading-relaxed text-white'>
+                <p className='mt-8 text-[18px] text-justify sm:text-[15px] max-md:text-[14px] leading-relaxed text-white'>
                     We believe that the global world is constantly evolving, triggering a whole new process in its wake that impacts every company. For organisations to perform better and achieve business success in the ever-evolving and globalized world, we aid them by designing innovative, strategic and practical solutions to the challenges facing their organisations. 
                 </p>
             </div>
-            <div className=' justify-center items-center max-md:h-[350px] h-[370px] md:h-[420px] w-[330px] md:w-[100%]' >
+            <div className=' justify-center items-center max-md:h-[350px] h-[370px] md:h-[420px] w-[390px] md:w-[100%]' >
                 <Image src={img} alt='/' className='rounded-se-3xl rounded-bl-3xl object-cover w-full h-full'/>
             </div>
         </div>
         
         {/* second sess */}
         <div className='mt-8'>
-            <p className='text-justify max-md:text-[14px] leading-relaxed text-white'>
+            <p className='text-justify text-[18px] sm:text-[15px] max-md:text-[14px] leading-relaxed text-white'>
                 A-Class Academic Consults Limited is Corporate Affairs Commission (CAC) registered and has national affiliations with experts, industry leaders and organisations highly committed to harnessing the potential and synergies of the national and global economy.  Furthermore, We offer research packages integrated with sophisticated technologies, personnel and human development training, research and business consultancy services through our network of experienced industry and academic professionals knowledgeable in project management, innovative technologies, applied research, development, business management etc. The result is years of offering efficient, effective and strategic business solutions that drive the growth of firms in the public and private sectors. 
             </p>
         </div>
