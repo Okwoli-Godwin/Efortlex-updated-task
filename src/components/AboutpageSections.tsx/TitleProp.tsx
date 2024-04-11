206fcff4959180b82779ad904b23b24215956c4c
Okwoli-Godwin <okwolig60@gmail.com>
2024-04-11 09:53:54 +0100

    update

--- src/components/AboutpageSections.tsx/TitleProp.tsx
+++ src/components/AboutpageSections.tsx/TitleProp.tsx
@@ -10,7 +10,7 @@ const TitleProp: React.FC<TitleDescProps> = ({ title, description, titleColor}) 
   return (
     <div >
       <h2 className={`${titleColor} font-bold text-2xl max-md:text-xl `}>{title}</h2>
-      <p className={` md:w-[100%] sm:w-[100%] pt-3 text-white`}>{description}</p>
+      <p className={` md:w-[100%] sm:w-[100%] pt-3 text-white sm:text-justify`}>{description}</p>
     </div>
   )
 }
